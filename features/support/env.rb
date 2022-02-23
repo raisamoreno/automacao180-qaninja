35946e140baec0cc987e1925c657b08c6fe2b5a0
Raisa Moreno <moreno.raisa@gmail.com>
2022-02-23 15:35:38 -0300

    Web Automation II - aulas 17 a 20

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,10 +1,16 @@
 require "capybara"
 require "capybara/cucumber"
 require "faker"
+require "allure-cucumber"
 
 
 Capybara.configure do |config|
     config.default_driver = :selenium_chrome
     config.app_host = "http://rocklov-web:3000"
     config.default_max_wait_time = 10
+end
+
+AllureCucumber.configure do |config|
+    config.results_directory = "/logs"
+    config.clean_results_directory = true
 end
